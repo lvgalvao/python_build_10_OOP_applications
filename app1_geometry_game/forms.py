eb6636d24a7807413262716ff0fc2fcb06661de5
Luciano Filho <lvgalvaofilho@gmail.com>
2023-01-28 14:26:43 -0300

    update app1

--- app1_geometry_game/forms.py
+++ app1_geometry_game/forms.py
@@ -7,7 +7,9 @@ class Point:
     def printer(self):
         return print(self.x, self.y)
 
-class Rectangule:
-    def __init__(self, x: Point, y):
-        self.x = x
-        self.y = y
+    def falls_in_rectangle(self, lowleft, upright):
+        if lowleft[0] < self.x < upright[0] \
+        and lowleft[1] < self.y < upright[1]:
+            return True
+        else:
+            return False
